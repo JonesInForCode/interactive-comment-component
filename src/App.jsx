9f25aeab47d7c9886cc6e40101aad74a4a0314d5
Nick Jones <nico.rjones@outlook.com>
2024-04-26 08:07:21 -0700

    starting dev server framework

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,37 @@
 import React from 'react'
 import Comment from './components/Comment.jsx'
 import './App.css'
+import services from './services/services.js'
 
 function App() {
+
+  // ** State Hooks ** //
+  const [comments, setComments] = React.useState([])
+  const [newComment, setNewComment] = React.useState('')
+  const [showAll, setShowAll] = React.useState(true)
+
+  // ** useEffect Hooks ** //
+React.useEffect(() => {
+  services
+    .getAll()
+    .then(response => {
+      setComments(response.data)
+    })
+}, [])
+
+const addComment = (event) => {
+  event.preventDefault()
+  const commentObject = {
+    content: newComment,
+  }
+
+  services
+    .create(commentObject)
+    .then(response => {
+      setComments(comments.concat(response.data))
+      setNewComment('')
+    })
+}
 
   return (
     <React.Fragment>
